961b56b50ae0702ec0755cb705a8f703246bb888
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-09-30 20:53:13 -0300

    feat: add styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,21 +2,17 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header>
-      <ul>
-        <li>
-          <Link href={"/"}>Home</Link>
-        </li>
-        <li>
+    <div className="w-full absolute text-white z-10">
+      <nav className="container relative flex flex-wrap items-center justify-between mx-auto p-8">
+        <Link href={"/"} className="font-bold text-3xl">
+          Home
+        </Link>
+        <div className="space-x-4 text-xl">
           <Link href={"/performance"}>Performance</Link>
-        </li>
-        <li>
           <Link href={"/reliability"}>Reliability</Link>
-        </li>
-        <li>
           <Link href={"/scale"}>Scale</Link>
-        </li>
-      </ul>
-    </header>
+        </div>
+      </nav>
+    </div>
   );
 }
